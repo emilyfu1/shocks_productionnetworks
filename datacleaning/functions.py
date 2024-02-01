22d496898bbd9cc3bbb9906e7b311ca4212a48d4
Emily Fu <fuem@bocdaz.ca>
2024-02-01 14:27:54 -0500

    import numpy

--- datacleaning/functions.py
+++ datacleaning/functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from dotenv import dotenv_values, find_dotenv
